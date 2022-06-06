bdf3bc0f0204cacf9c2bb344540093dfe477da59
ANJELA <91777306+Angel0926@users.noreply.github.com>
2022-06-07 01:25:42 +0400

    Config sequrity, Jwt Token util,filter

---
+++ src/main/java/com/example/cafemanagementsystem/security/CurrentUser.java
@@ -0,0 +1,19 @@
+package com.example.cafemanagementsystem.security;
+
+
+import com.example.cafemanagementsystem.domain.entity.User;
+import org.springframework.security.core.authority.AuthorityUtils;
+
+public class CurrentUser extends org.springframework.security.core.userdetails.User {
+
+    private final User user;
+
+    public CurrentUser(User user) {
+        super(user.getEmail(),user.getPassword(), AuthorityUtils.createAuthorityList(user.getRoleType().name()));
+        this.user = user;
+    }
+
+    public User getUser(){
+        return user;
+    }
+}
