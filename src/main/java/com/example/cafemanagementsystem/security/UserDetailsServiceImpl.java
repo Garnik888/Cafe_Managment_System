cf157a65ad826bef00f90182824f0d9cef06326f
ANJELA <91777306+Angel0926@users.noreply.github.com>
2022-06-11 06:35:58 +0400

    create service

--- src/main/java/com/example/cafemanagementsystem/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/cafemanagementsystem/security/UserDetailsServiceImpl.java
@@ -1,22 +1,19 @@
 package com.example.cafemanagementsystem.security;
 
 import com.example.cafemanagementsystem.domain.entity.User;
-import com.example.cafemanagementsystem.repository.UserRepo;
+import com.example.cafemanagementsystem.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
-import java.util.Collection;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
-    private final UserRepo userRepository;
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
